aaeafdfd2205f111a88e981ee335aad75da74e2d
Dave Paul <david.paul@showtime.net>
2017-01-25 13:14:38 -0500

    added body-parser for POSTs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({'extended': true}))
 
 app.listen(3000, function() {
   console.log('listening on 3000')
@@ -11,4 +14,5 @@ app.get('/', (req, res) => {
 
 app.post('/quotes', (req, res) => {
   console.log('new quote incoming');
+  console.log(req.body)
 })
