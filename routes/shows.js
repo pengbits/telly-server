06bf9520b829e5e2764cb4af76ae1a3fbb81ad8c
Dave Paul <david.paul@showtime.net>
2017-01-25 16:55:47 -0500

    cleanup

--- routes/shows.js
+++ routes/shows.js
@@ -14,35 +14,55 @@ db.open(function(err, db) {
 
 exports.findAll = (req,res) => {
   db.collection('shows').find().toArray((err,results) => {
-    res.render('index.ejs', {shows: results})
+    res.json({shows: results})
   })
 };
 
 exports.findById = (req,res) => {
   var id = req.params.id;
+  var response = {};
   console.log('Retrieving show: ' + id);
-  db.collection('shows').findOne({'_id':BSON.ObjectID(id)}, function(err, item) {
-    res.render('show.ejs', {show: item})
-  });
-};
-
-exports.editShow = (req,res) => {
-  var id = req.params.id;
-  console.log('Retrieving show: ' + id);
-  db.collection('shows').findOne({'_id':BSON.ObjectID(id)}, function(err, item) {
-    res.render('edit.ejs', {show: item})
+  
+  db.collection('shows').findOne({
+    '_id':BSON.ObjectID(id)
+  }, 
+  function(err, item) {
+    if(err || item==null) {
+      response = {
+        'error' : true, 
+        'message' : 'Error fetching data'
+      };
+    } else {
+      response = {
+        'error' : false, 
+        'show' : item
+      }
+    }
+    res.json(response)
   });
 };
 
 exports.addShow = (req, res) => {
 
-  var show = req.body;
+  var show = {};
+  var response = {};
+  show.name     = req.body.name;
+  show.network  = req.body.network;
   console.log('Adding Show: ' + JSON.stringify(show));
   
-  db.collection('shows').save(show, {safe: true}, function(err, result) {
-    if (err) return console.log('An error has occurred');
-
-    console.log('Success: ' + JSON.stringify(result[0]));
-    res.redirect('/')
+  db.collection('shows').save(show, {safe: true}, 
+  function(err, result) {
+    if(err) {
+      response = {
+        'error' : true, 
+        'message' : 'Error adding data'
+      };
+    } else {
+      response = {
+        'error' : false, 
+        'message' : 'Data added'
+      };
+    }
+    res.json(response);
   });
 };
--- server.js
+++ server.js
@@ -7,17 +7,12 @@ const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': true}))
 
-router.get('/', function (req,res){
-  res.json({'error':false, 'mesage':'hi there'})
-})
-app.use('/', router);
-
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
 
-// app.get('/',          shows.findAll);
-// app.get('/shows',     shows.findAll);
-// app.get('/shows/:id', shows.findById);
+app.get('/',          shows.findAll);
+app.get('/shows',     shows.findAll);
+app.get('/shows/:id', shows.findById);
 // app.get('/shows/(:id)/edit', shows.editShow);
 // app.post('/shows',    shows.addShow);
