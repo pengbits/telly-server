e5144fe02b2657d8f01b8aa11a4421e1a6bee520
Dave Paul <david.paul@showtime.net>
2017-02-15 16:35:50 -0500

    restored access-control headers

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': true}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(3000, function() {
   console.log('listening on 3000')
